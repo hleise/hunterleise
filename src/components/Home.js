dce6f15a05afa2ca3e32cc7f394da2fba85f7ac2
Hunter Leise <hleise27@gmail.com>
2018-01-09 23:12:00 -0700

    Update build

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,18 +15,7 @@ export default function Home() {
 
   return (
     <div>
-      <p style={s.p}>
-        This is an example single page app built
-        with React and React&nbsp;Router using {' '}
-        <Code>BrowserRouter</Code>. Navigate with the links below and
-        refresh the page or copy/paste the url to test out the redirect
-        functionality deployed to overcome GitHub&nbsp;Pages incompatibility
-        with single page apps (like this one).
-      </p>
-      <p style={s.p}>
-        Please see the {repoReadmeLink('repo readme')} for instructions on how to
-        use this boilerplate to deploy your own single page app using GitHub Pages.
-      </p>
+      <h1>Coming Soon!</h1>
       <div style={s.pageLinkContainer}>
         <Interactive
           as={Link}
