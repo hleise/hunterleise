38b2e48d3153118656f3fdce4b63abd19d586821
Hunter Leise <hleise27@gmail.com>
2018-01-12 02:50:12 -0700

    CSS not working...

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="home-page">
       <div id="content-container">
-        <p>Hi! My Name Is</p>
+        <p>Hi, my name is</p>
         <h1>Hunter Leise</h1>
       </div>
     </div>
