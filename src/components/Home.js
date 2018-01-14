b1ff29589dd08c40e03907e7bde05be0c31c262d
Hunter Leise <hleise27@gmail.com>
2018-01-13 21:44:06 -0700

    Add description to home page.

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,9 @@ export default function Home() {
   return (
     <div className="home-page">
       <div id="content-container">
-        <p>Hi, my name is</p>
-        <h1>Hunter Leise</h1>
+        <p className="introduction">Hi, my name is</p>
+        <h1 className="introduction">Hunter Leise</h1>
+        <p className="description">I'm a student, developer, and aspiring entrepreneur.</p>
       </div>
     </div>
   );
