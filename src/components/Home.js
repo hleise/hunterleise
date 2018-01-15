e547f72550ba42e0bb2ada0d741c49c4152f3b41
Hunter Leise <hleise27@gmail.com>
2018-01-14 18:04:56 -0700

    Add navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import NavBar from './NavBar';
 
 export default function Home() {
   return (
     <div className="home-page">
+      <NavBar />
       <div id="content-container">
         <p className="introduction">Hi, my name is</p>
         <h1 className="introduction">Hunter Leise</h1>
---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { elastic as Menu } from 'react-burger-menu';
+
+export default function NavBar() {
+  return (
+    <Menu>
+      <Link to="/" className="menu-item">
+        <i className="fa fa-home" aria-hidden="true" />
+        <span className="menu-item-text">Home</span>
+      </Link>
+      <Link to="/about" className="menu-item">
+        <i className="fa fa-info-circle" aria-hidden="true" />
+        <span className="menu-item-text">About</span>
+      </Link>
+      <Link to="/contact" className="menu-item">
+        <i className="fa fa-address-book" aria-hidden="true" />
+        <span className="menu-item-text">Home</span>
+      </Link>
+    </Menu>
+  );
+}
